68ddcaae11ec656589e5f1b0e309fffde3a1348f
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-28 20:16:46 -0500

    Clean up code.

---
+++ SportsCar.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Remoting.Contexts;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace ObjectContextApp
+{
+    // SportsCar has no special contextual
+    // needs and will be loaded into the
+    // default context of the app domain.
+    class SportsCar
+    {
+        public SportsCar()
+        {
+            // Get context information and print out context ID.
+            Context context = CurrentContext;
+            WriteLine($"{ToString()} object in context {context.ContextID}.");
+            foreach (IContextProperty iContextProperty in context.ContextProperties)
+                WriteLine($"-> Ctx Prop: {iContextProperty.Name}.");
+        }
+    }
+}
---
+++ SportsCarThreadSafe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace ObjectContextApp
+{
+    // SportsCarTS demands to be loaded in
+    // a synchronization context.
+    [Synchronization]
+    class SportsCarThreadSafe : ContextBoundObject
+    {
+        public SportsCarThreadSafe()
+        {
+            // Get context information and print out context ID.
+            Context context = CurrentContext;
+            WriteLine($"{ToString()} object in context {context.ContextID}.");
+            foreach (IContextProperty iContextProperty in context.ContextProperties)
+                WriteLine($"-> Context Property: {iContextProperty.Name}.");
+        }
+    }
+}
